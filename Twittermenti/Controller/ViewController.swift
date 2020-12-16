db81e7a0edbb624896cc487336149351f18e11ba
Nico Cobelo <ingcobelo@gmail.com>
2020-12-16 18:40:45 -0300

    Add Sentiment Classification

--- Twittermenti/Controller/ViewController.swift
+++ Twittermenti/Controller/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    let sentimentClassifier = TweetSentimentClassifier()
     
     let swifter = Swifter(consumerKey: APIKeys.mainKey, consumerSecret: APIKeys.secretKey)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let prediction = try! sentimentClassifier.prediction(text: "@Apple is the best company")
+        
+        print(prediction.label)
+        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results, metadata) in
+            //print(results)
+        } failure: { (error) in
+            //print("There was an error with the Twitter API Request, \(error)")
+        }
+
     }
 
     @IBAction func predictPressed(_ sender: Any) {
